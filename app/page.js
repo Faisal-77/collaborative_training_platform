fc07bd53c808af2aa467fe4a6dff6027a21fca87
Faisal-77 <76266938+Faisal-77@users.noreply.github.com>
2024-01-04 02:46:39 +0300

    " "

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import styles from './page.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/home_page/header.jsx';
+import Nav from './components/home_page/nav.jsx';
+import  {Content , Under_content} from './components/home_page/conent';
+import {Footer , Footer_2} from './components/home_page/footer';
+
+export default function Home() {
+  return (
+  <div className={`container-fluid text-center`}>
+      <div className={styles.contaner}>
+          <Header />
+          <Nav />
+      </div>
+      <Content />
+      <Footer />
+      <Footer_2 />
+  </div>
+  )
+}
