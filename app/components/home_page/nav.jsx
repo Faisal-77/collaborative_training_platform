4e423686b9e74614943f6827388a16fb24d81b05
Faisal-77 <76266938+Faisal-77@users.noreply.github.com>
2024-01-10 17:07:12 +0300

    THE LAST UPDATE IN AUTH

--- app/components/home_page/nav.jsx
+++ app/components/home_page/nav.jsx
@@ -17,11 +17,8 @@ export default function nav() {
         <Link href={'/'}>  <span className={styles.text_nav}>  الصفحة الرائيسية </span> </Link>
         </div>
         <div className={`col `}>      
-<<<<<<< HEAD
           <Link href={'/about'}>  <span className={styles.text_nav}> جهاة التدريب </span> </Link>
-=======
           <Link href={'/'}>  <span className={styles.text_nav}> جهاة التدريب </span> </Link>
->>>>>>> b232140c822ac6976de930c30327e8f028ec6b7e
         </div>
         <div className="col ">
             <Link href={'/'}>  <span className={styles.text_nav}>الأقتراحات</span> </Link>
--- app/page.js
+++ app/page.js
@@ -3,19 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/home_page/header.jsx';
 import Nav from './components/home_page/nav.jsx';
 import  {Content , Under_content} from './components/home_page/conent';
-<<<<<<< HEAD
 import {Footer , Footer_2} from './components/home_page/footer'
 export default function Home() {
   return (
    <div className={`container-fluid text-center`}>
   
-=======
-import {Footer , Footer_2} from './components/home_page/footer';
-
-export default function Home() {
-  return (
-  <div className={`container-fluid text-center`}>
->>>>>>> b232140c822ac6976de930c30327e8f028ec6b7e
       <div className={styles.contaner}>
           <Header />
           <Nav />
@@ -23,11 +15,7 @@ export default function Home() {
       <Content />
       <Footer />
       <Footer_2 />
-<<<<<<< HEAD
       
    </div>
-=======
-  </div>
->>>>>>> b232140c822ac6976de930c30327e8f028ec6b7e
   )
 }
